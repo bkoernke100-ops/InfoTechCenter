a1ba8eac53291f3d0e93baee7faf6d0215d62f7f
Koernke <bkoernke100@gmail.com>
2026-01-14 12:21:24 -0500

    While Loop ... 95% stable doing research

--- WithOrWithOutMarinar.py
+++ WithOrWithOutMarinar.py
@@ -1,5 +1,23 @@
 #Welcome Branch
+
+#Libraries Imported Here
+import sys
+import time
  
 print("\nWelcome Branch - Develpoer: Brennen Koernke")
 print("\nWelcome to Info Tech Center v.1.0")
-print("\nInfo Tech Center is Booting up...")
+
+x = 0
+ellipsis = 0
+
+while x !=20:
+    x += 1
+    ellipsisMessage = ("InfoTechCenter OS Booting" + "." * ellipsis)
+    ellipsis += 1
+    sys.stdout.write("\r" + ellipsisMessage + "   ")
+    sys.stdout.flush()
+    time.sleep (.5)
+    if ellipsis == 4:
+        ellipsis = 0
+    if x == 20:
+        print("\nOperating System Booted Up - Retina Scanned - Access Granted")
