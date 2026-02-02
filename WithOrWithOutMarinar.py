ab203416c21e4f63e2fa47e8dc539715da0db56f
Koernke <bkoernke100@gmail.com>
2026-02-02 14:19:26 -0500

    Able to detect when below 25% gas

--- WithOrWithOutMarinar.py
+++ WithOrWithOutMarinar.py
@@ -1,7 +1,14 @@
 #Gasoline  Branch
 
-#Gas Tank Level
+# Gasoline Branch
 import random 
 
-random_number = random.randint (1,100)
-print("Scaning gas tank...Scan complete gas tank level",random_number, "%")
+random_number = random.randint(1, 100)
+print("Scanning gas tank... Scan complete. Gas tank level:", random_number, "%")
+
+if random_number < 25:
+    print("Gas level below recommended level... please find the nearest gas station")
+else:
+    print("Gas level is sufficient")
+
+if random_number <25:
