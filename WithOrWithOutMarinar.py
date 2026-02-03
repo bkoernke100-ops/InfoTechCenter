ff7f6dcb5d17c931af8c791317ee764f20ce1e49
Brennen Koernke <bkoernke100@gmail.com>
2026-02-03 13:27:23 -0500

    Nearest gas station

--- WithOrWithOutMarinar.py
+++ WithOrWithOutMarinar.py
@@ -7,8 +7,15 @@ random_number = random.randint(1, 100)
 print("Scanning gas tank... Scan complete. Gas tank level:", random_number, "%")
 
 if random_number < 25:
-    print("Gas level below recommended level... please find the nearest gas station")
+    print("Gas level below recommended level... please find a local gas station")
 else:
     print("Gas level is sufficient")
 
 if random_number <25:
+    import random
+
+    words = ["Cicle K", "The Local gas station", "Gas", "date", "elderberry"]
+
+    chosen_words = random.sample(words, 3)
+
+    print(chosen_words)
